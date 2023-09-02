5e6884e78c4cd96f45929587b13585388e82d341
Eduard <IbadToff@gmail.com>
2023-09-02 22:38:52 +0300

    vercel

--- config/db.js
+++ config/db.js
@@ -2,15 +2,19 @@ require('dotenv').config();
 const pg = require('pg');
 const Sequelize = require('sequelize');
 
-const pgDb = new Sequelize(
-    'youtubespa', 
-    'postgres', 
-    'admin', 
-    {
-        host: 'localhost', 
-        dialect: 'postgres',
-        dialectModule: pg
-    }
-)
+// const pgDb = new Sequelize(
+//     'youtubespa', 
+//     'postgres', 
+//     'admin', 
+//     {
+//         host: 'localhost', 
+//         dialect: 'postgres',
+//         dialectModule: pg
+//     }
+// );
+
+const pgDb = new Sequelize('postgres://admin:admin@localhost:5432/youtubespa', {
+  dialectModule: pg
+});
 
 module.exports = pgDb;
