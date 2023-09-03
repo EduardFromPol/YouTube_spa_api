e00c7816d93cc7695d0bb8eba8337e6652dd5140
Eduard <IbadToff@gmail.com>
2023-09-03 19:10:10 +0300

    deploy

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-// const cors = require('cors');
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
