6efcf4f069983be61a14d82bd7c87dd1c75fa2f8
Eduard <IbadToff@gmail.com>
2023-09-03 18:21:48 +0300

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
