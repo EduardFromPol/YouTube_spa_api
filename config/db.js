35bff9e6f4703b71fd164b04ab8799b5533fc5ca
Eduard <IbadToff@gmail.com>
2023-09-03 18:30:04 +0300

    deploy

--- config/db.js
+++ config/db.js
@@ -2,19 +2,15 @@ require('dotenv').config();
 const pg = require('pg');
 const Sequelize = require('sequelize');
 
-// const pgDb = new Sequelize(
-//     'youtubespa', 
-//     'postgres', 
-//     'admin', 
-//     {
-//         host: 'localhost', 
-//         dialect: 'postgres',
-//         dialectModule: pg
-//     }
-// );
-
-const pgDb = new Sequelize('postgres://admin:admin@localhost:5432/youtubespa', {
-  dialectModule: pg
-});
+const pgDb = new Sequelize(
+    'youtubespa', 
+    'postgres', 
+    'admin', 
+    {
+        host: 'localhost', 
+        dialect: 'postgres',
+        dialectModule: pg
+    }
+);
 
 module.exports = pgDb;
