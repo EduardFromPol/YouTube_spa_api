a633926a1efbdf698f4a6a1ad0404533f497b90c
Eduard <IbadToff@gmail.com>
2023-09-07 13:50:44 +0300

    seqbranch

--- config/db.js
+++ config/db.js
@@ -4,6 +4,22 @@ const Sequelize = require('sequelize');
 
 // const pgDb = new Sequelize(process.env.POSTGRES_CONNECT_URL); // local db
 // const pgDb = new Sequelize(process.env.POSTGRES_URL); // vercel db
-const pgDb = new Sequelize(process.env.POSTGRES_RENDER_URL) // render db
+// const pgDb = new Sequelize(process.env.POSTGRES_RENDER_URL) // render db
+
+const pgDb = new Sequelize(
+    'youtube_api_seq', 
+    'youtube_api_seq_user', 
+    'CsNFVwiyeAcuMedjk0fkWEtWLuLvYGTy', 
+    {
+        host: 'ep-bold-frost-03649591-pooler.us-east-1.postgres.vercel-storage.com',
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+            }
+        }
+    }
+)
 
 module.exports = pgDb;
