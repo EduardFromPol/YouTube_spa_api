26ffde1be0c4a2e2f92a7275e646f6d921186662
Eduard <IbadToff@gmail.com>
2023-09-07 12:14:54 +0300

    seqbranch

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 
 
 // const pgDb = new Sequelize(process.env.POSTGRES_CONNECT_URL); // local db
-const pgDb = new Sequelize(process.env.POSTGRES_URL); // vercel db
+// const pgDb = new Sequelize(process.env.POSTGRES_URL); // vercel db
+const pgDb = new Sequelize(process.env.POSTGRES_RENDER_URL) // render db
 
 module.exports = pgDb;
