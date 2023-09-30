600c328516c64acce8c90bf17bae68ba556b8c92
Eduard <ibadtoff@gmail.com>
2023-09-30 14:37:04 +0300

    fixed

--- config/db.js
+++ config/db.js
@@ -2,27 +2,27 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 
-const pgDb = new Sequelize(process.env.POSTGRES_CONNECT_URL); // local db
+// const pgDb = new Sequelize(process.env.POSTGRES_CONNECT_URL); // local db
 // const pgDb = new Sequelize(process.env.POSTGRES_URL); // vercel db
 // const pgDb = new Sequelize(process.env.POSTGRES_RENDER_URL) // render db
 
 const { DBNAME, DBUSERNAME, DBPASSWORD, DBHOST } = process.env;
 
-// const pgDb = new Sequelize(
-//     DBNAME, 
-//     DBUSERNAME, 
-//     DBPASSWORD, 
-//     {
-//         host: DBHOST,
-//         dialect: 'postgres',
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//                 rejectUnauthorized: false // <<<<<<< YOU NEED THIS
-//             }
-//         }
-//     }
-// );
+const pgDb = new Sequelize(
+    DBNAME, 
+    DBUSERNAME, 
+    DBPASSWORD, 
+    {
+        host: DBHOST,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+            }
+        }
+    }
+);
 
 
 
